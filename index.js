d2a92c74e717d28092a2fea7775910b15a4e6aa9
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-04-05 15:52:34 -0700

    wrote add function and getList function

---
+++ index.js
@@ -0,0 +1,44 @@
+class BinaryTreeNode {
+
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+  add(node) {               //add will add a new node
+    if (!this.next) {       //if currently node, will move to next node
+      this.next = node;  
+    } else {
+      this.next.add(node);  //if no node, will add node
+    }
+  }
+
+  getList() {                               //added the getList to bottom of code to see console log results
+    if(!this.next) return this.value;
+    return `${this.value} ${this.next.getList()}`;
+    
+  }
+}
+    
+const B = new BinaryTreeNode('B');
+const A = new BinaryTreeNode('A');
+const C = new BinaryTreeNode('C');
+const D = new BinaryTreeNode('D');
+  
+// B will be the root of the tree:
+B.add(A);
+B.add(D);
+B.add(C);
+  
+console.log(B);
+
+const root = new BinaryTreeNode('A');
+const nodeB = new BinaryTreeNode('B');
+root.add(nodeB);
+console.log(root.getList()); // 'A B'
+const nodeC = new BinaryTreeNode('C');
+const nodeD = new BinaryTreeNode('D');
+const nodeE = new BinaryTreeNode('E');
+root.add(nodeC);
+root.add(nodeD);
+root.add(nodeE);
+console.log(root.getList()); 
