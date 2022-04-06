9d2b85a33369e2234cecc185ab75f0a4a2919cfb
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-04-05 17:59:56 -0700

    final attempt at findPerson. no luck

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ class BinaryTreeNode {
   constructor(value) {
     this.value = value;
     this.next = null;
+    this.left = null;
+    this.right = null;
   }
-  add(node) {               //add will add a new node
-    if (!this.next) {       //if currently node, will move to next node
-      this.next = node;  
+  add(node) {
+    if (node.value === this.value) {
+      return;
+    }
+    if(node.value < this.value) {
+      if(!this.left) this.left = node;
+      else this.left.add(node);  
     } else {
-      this.next.add(node);  //if no node, will add node
+      if(!this.right) this.right = node;
+      else this.right.add(node); 
     }
   }
 
@@ -42,3 +49,57 @@ root.add(nodeC);
 root.add(nodeD);
 root.add(nodeE);
 console.log(root.getList()); 
+
+class PersonTreeNode {
+  constructor(person) {
+    this.value = person.name;
+    this.person = person;
+    this.left = null;
+    this.right = null;
+  }
+    
+  add(node) {
+    if (node.value === this.value) {
+      return;
+    }
+    if(node.value < this.value) {
+      if(!this.left) this.left = node;
+      else this.left.add(node);  
+    } else {
+      if(!this.right) this.right = node;
+      else this.right.add(node); 
+    }
+  }
+    
+  //If the name is not equal to person name return that name
+  // Not sure if this is correct. Modeled after the getlist function
+  findPerson(name) {
+    if(this.value === name) return this.person;
+    return this.person.name.findPerson();
+  }
+}
+
+  
+const p1 = new BinaryTreeNode('Nelson');
+const p2 = new BinaryTreeNode('Smith');
+const p3 = new BinaryTreeNode('Brook');
+const p4 = new BinaryTreeNode('Dude');
+  
+// B will be the root of the tree:
+p1.add(p2);
+p1.add(p3);
+p1.add(p4);
+  
+console.log(p1);
+  
+const Nelson = { 
+  name: 'Nelson', 
+  phone: '555-1212', 
+  address: '123 Main St' 
+};
+  
+const person = findPerson('Nelson');
+  
+  
+const person2 = root.findPerson('Foo');
+// person2 is null
